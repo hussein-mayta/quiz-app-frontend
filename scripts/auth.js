efa821fb93d45d61706ae572da926f7ad896fdd5
Hussein <husseinmayta@gmail.com>
2025-04-21 12:24:08 +0300

    Collect form input values

---
+++ scripts/auth.js
@@ -0,0 +1,9 @@
+
+document.getElementById("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const message = document.getElementById("message");
+
