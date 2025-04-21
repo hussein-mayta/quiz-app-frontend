1fc5dd8edcfecdee38c6162ec78d7005e74df293
Hussein <husseinmayta@gmail.com>
2025-04-21 12:37:17 +0300

    Collect input values from registration form

---
+++ scripts/reg.js
@@ -0,0 +1,9 @@
+document.getElementById("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const nameInput = document.getElementById("name").value.trim();
+    const emailInput = document.getElementById("email").value.trim();
+    const passwordInput = document.getElementById("password").value.trim();
+
+    const message = document.getElementById("message");
+
