9199feadf0cfda28774213cdb70b558c61de00dc
Hussein <husseinmayta@gmail.com>
2025-04-21 12:47:18 +0300

    Add default quiz data

---
+++ scripts/home.js
@@ -0,0 +1,34 @@
+
+// use it to clear stored data => localStorage.clear();
+const defaultQuizzes = [
+    {
+      key: "quiz_worldcup",
+      title: "World Cup History",
+      description: "Test your football knowledge…",
+      image: "../assets/images/worldcup.png",
+      url: "../pages/quiz.html"
+    },
+    {
+      key: "quiz_generalInfo",
+      title: "General Knowledge",
+      description: "Challenge your brain with fun facts…",
+      image: "../assets/images/general-info.png",
+      url: "../pages/quiz.html"
+    },
+    {
+      key: "quiz_geography",
+      title: "Where in the World?",
+      description: "Think you know the planet? Let's find out!",
+      image: "../assets/images/geography.png",
+      url: "../pages/quiz.html"
+    },
+    {
+      key: "quiz_englishLanguage",
+      title: "English Challenge",
+      description: "From tricky words to grammar rules…",
+      image: "../assets/images/english-lang.png",
+      url: "../pages/quiz.html"
+    }
+  ];
+  
+ 
